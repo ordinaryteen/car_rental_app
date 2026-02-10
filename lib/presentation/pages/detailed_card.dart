6ceb329cee3c1ab9895aa66b371ef5e2dc3f2fa8
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-02-10 20:20:54 +0700

    create the detailed page UI and make the widget for mocked 'more section' card

--- lib/presentation/pages/detailed_card.dart
+++ lib/presentation/pages/detailed_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:car_rental_app/data/models/car.dart';
+import 'package:car_rental_app/presentation/widgets/more_card.dart';
 
 class CarDetailScreen extends StatelessWidget {
   final Car car;
@@ -10,6 +11,7 @@ class CarDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -17,105 +19,188 @@ class CarDetailScreen extends StatelessWidget {
             Text(' Information'),
           ],
         ),
+        actions: const [
+          Opacity(
+            opacity: 0.0,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Icon(Icons.arrow_back),
+            ),
+          ),
+        ],
       ),
       body: Column(
         children: [
-          Container(
-            height: 150,
-            child: Image.asset('assets/car_image.png'),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            car.model,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.directions_car,
-                    color: Colors.blueGrey,
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    '${car.distance.toStringAsFixed(0)}km',
-                    style: TextStyle(color: Colors.blueGrey, fontSize: 13),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(
-                    Icons.local_gas_station,
-                    color: Colors.blueGrey,
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    '${car.fuelCapacity.toStringAsFixed(0)}L',
-                    style: TextStyle(color: Colors.blueGrey, fontSize: 13),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          Expanded(
+          // Part 1: Car Image and Description
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
             child: Container(
-              padding: EdgeInsets.all(20),
+              height: 225,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 10,
+                    spreadRadius: 5,
+                  )
+                ],
+              ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  SizedBox(
+                    height: 100,
+                    child: Image.asset(
+                      'assets/car_image.png',
+                      height: 100,
+                    ),
+                  ),
+                  Text(
+                    car.model,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      CircleAvatar(
-                        radius: 40,
-                        backgroundImage: AssetImage('assets/user.png'),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      Row(
                         children: [
-                          Text(
-                            'Jane Cooper',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
+                          const Icon(
+                            Icons.directions_car,
+                            color: Colors.blueGrey,
+                          ),
+                          const SizedBox(
+                            width: 5,
                           ),
                           Text(
-                            '\$4,253',
-                            style: TextStyle(color: Colors.grey),
+                            '${car.distance.toStringAsFixed(0)}km',
+                            style: const TextStyle(
+                                color: Colors.blueGrey, fontSize: 13),
                           ),
                         ],
-                      )
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.local_gas_station,
+                            color: Colors.blueGrey,
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            '${car.fuelCapacity.toStringAsFixed(0)}L',
+                            style: const TextStyle(
+                                color: Colors.blueGrey, fontSize: 13),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                  SizedBox(
-                    height: 20,
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+
+          // Part 2: User Info and Maps
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color(0xffF3F3F3),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10,
+                          spreadRadius: 5,
+                        )
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        const CircleAvatar(
+                          radius: 40,
+                          backgroundImage: AssetImage('assets/user.png'),
+                        ),
+                        const SizedBox(height: 10),
+                        const Text(
+                          'Jane Cooper',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const Text(
+                          '\$4,253',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
                   ),
-                  Expanded(
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child: Container(
+                    height: 170,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffF3F3F3),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10,
+                          spreadRadius: 5,
+                        )
+                      ],
+                    ),
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(20),
                       child: Image.asset(
                         'assets/maps.png',
                         fit: BoxFit.cover,
-                        width: double.infinity,
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          )
+          ),
+
+          // Part 3: More Cars
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                MoreCard(
+                    car: Car(
+                        model: "${car.model}-1",
+                        distance: car.distance + 100,
+                        fuelCapacity: car.fuelCapacity + 100,
+                        pricePerHour: car.pricePerHour + 10)),
+                const SizedBox(height: 5),
+                MoreCard(
+                    car: Car(
+                        model: "${car.model}-2",
+                        distance: car.distance + 200,
+                        fuelCapacity: car.fuelCapacity + 200,
+                        pricePerHour: car.pricePerHour + 20)),
+                const SizedBox(height: 5),
+                MoreCard(
+                    car: Car(
+                        model: "${car.model}-3",
+                        distance: car.distance + 300,
+                        fuelCapacity: car.fuelCapacity + 300,
+                        pricePerHour: car.pricePerHour + 30)),
+              ],
+            ),
+          ),
         ],
       ),
     );
