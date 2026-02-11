9be2a27b2771259b10a0eedce84147d919fd282c
nafiMulyo <muhamadnafimulyo@gmail.com>
2026-02-11 09:44:42 +0700

    fix the path from a widget

--- lib/presentation/pages/onboarding.dart
+++ lib/presentation/pages/onboarding.dart
@@ -1,7 +1,6 @@
 import 'package:car_rental_app/presentation/pages/car_list.dart';
 import 'package:flutter/material.dart';
-
-import 'package:car_rental_app/presentation/widgets/car_rental_button.dart';
+import 'package:car_rental_app/presentation/widgets/_button.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
